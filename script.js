5ebc2f2f13b9a78a7f17499c4eeba0c0a8ac5dd6
Drina <drinamusaj01@gmail.com>
2025-04-22 09:12:53 +0200

    fixed popups

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const navMenu = document.querySelector('.nav-menu');
 const images = document.querySelectorAll('.projectItem');
 const popupbox = document.querySelector('.popup');
 const closeBtn = document.querySelector('.close-btn');
+const modalInner = document.querySelector('.modal-inner');
 
 const sendBtn = document.querySelector('.form-submit-btn');
 
@@ -21,12 +22,47 @@ document.querySelectorAll('.nav-link').forEach(n=> n.addEventListener("click",()
 }))
 
 
+const projectImg = {
+  project1: [
+    'images/project-test1.jpg',
+    'images/project-test2.jpg',
+    'images/project-test3.jpg',
+    'images/project-test4.jpg'
+  ],
+  project2: [
+    'images/foodApp.png',
+    'images/foodLanding.png'
+  ],
+  project3: [
+    'images/schoolApp.png',
+    'images/eduLanding.png'
+  ],
+  project4: [
+    'images/project4Test1.jpg',
+    'images/project4Test2.jpg',
+    'images/project4Test3.jpg'
+  ]
+};
+
+
 images.forEach(projectItem => {
       projectItem.addEventListener("click", () => {
-          popupbox.style.display = "flex";
-      });
+        const projectKey = projectItem.getAttribute('data-image');
+        const imgs = projectImg[projectKey];
     
-  });
+        modalInner.innerHTML = '';
+    
+        imgs.forEach(src => {
+          const img = document.createElement('img');
+          img.src = src;
+          img.alt = 'project image';
+          modalInner.appendChild(img);
+        });
+    
+        popupbox.style.display = "flex";
+      });
+    });
+    
 
   closeBtn.addEventListener("click", () => {
     popupbox.style.display = "none";
